91fe5039b6f5b046c40607a55c5429cc95b6b0c8
Kushagra <kkss420@gmail.com>
2019-04-18 18:05:22 +0530

    added entity state mixin

--- lib/entity_state.dart
+++ lib/entity_state.dart
@@ -1,27 +1,80 @@
 library entity_state;
 
 import 'package:built_collection/built_collection.dart';
-import 'package:built_value/built_value.dart';
-import 'package:built_value/serializer.dart';
 import 'package:entity_state/errors/method_not_overriden.dart';
 
-part 'entity_state.g.dart';
-
-abstract class EntityState<T, V, K, A>
-    implements Built<EntityState<T, V, K, A>, EntityStateBuilder<T, V, K, A>> {
+mixin EntityState<T, V, K, B> {
   BuiltList<V> get ids;
 
   BuiltMap<V, T> get entities;
 
-  EntityState._();
+  K rebuild(updates(B));
 
-  factory EntityState([void Function(EntityStateBuilder<T, V, K, A>) updates]) =
-      _$EntityState<T, V, K, A>;
-
-  static Serializer<EntityState> get serializer => _$entityStateSerializer;
-
-  V getId() {
+  V getId(T data) {
     throw MethodNotOverridenError();
+  }
+
+  // Setter Methods
+
+  K addOne(T data) {
+    return rebuild((b) => b
+      ..ids = (ids.toBuilder()..add(getId(data))).build()
+      ..entities = (entities.toBuilder()
+            ..addAll(Map<V, T>.from({getId(data): data})))
+          .build());
+  }
+
+  K addAll(List<T> data) {
+    return rebuild((b) => b
+      ..ids = _getBuiltList(data).build()
+      ..entities = _getMapBuilder(data).build());
+  }
+
+  K addMany(List<T> data) {
+    return rebuild((b) => b
+      ..ids = (ids.toBuilder()..addAll(data.map((item) => getId(item)))).build()
+      ..entities = (entities.toBuilder()
+            ..addIterable(data,
+                key: (item) => getId(item), value: (item) => item))
+          .build());
+  }
+
+  K updateOne(T data) {
+    return rebuild((b) => b
+      ..entities = (entities.toBuilder()
+            ..updateValue(getId(data), (data) => data))
+          .build());
+  }
+
+  K updateMany(List<T> data) {
+    return rebuild((b) => b
+      ..entities = (entities.toBuilder()
+            ..addIterable(data,
+                key: (item) => getId(item), value: (item) => item))
+          .build());
+  }
+
+  K removeOne(V data) {
+    return rebuild((b) => b
+      ..ids = (ids.toBuilder()..removeWhere((id) => id == data)).build()
+      ..entities = (entities.toBuilder()
+            ..removeWhere((V key, T value) => key == data))
+          .build());
+  }
+
+  K removeMany(List<V> data) {
+    return rebuild((b) => b
+      ..ids =
+          (ids.toBuilder()..removeWhere((id) => data.indexOf(id) > -1)).build()
+      ..entities = (entities.toBuilder()
+            ..removeWhere((V key, T value) => data.indexOf(key) > -1))
+          .build());
+  }
+
+  K removeAll() {
+    return rebuild((b) => b
+      ..ids = []
+      ..entities = {});
   }
 
   // Getter Methods
@@ -40,5 +93,16 @@ abstract class EntityState<T, V, K, A>
 
   int getCount() {
     return ids.length;
+  }
+
+  // Helper Methods
+
+  ListBuilder<V> _getBuiltList(Iterable<T> list) {
+    return BuiltList<V>.from(list.map((item) => getId(item))).toBuilder();
+  }
+
+  MapBuilder<V, T> _getMapBuilder(List<T> list) {
+    return BuiltMap<V, T>.from(Map.fromIterable(list,
+        key: (item) => getId(item), value: (item) => item)).toBuilder();
   }
 }
